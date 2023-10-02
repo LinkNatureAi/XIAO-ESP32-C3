6fe27739051cf693987019cba93729a516483561
SONU KOL <sonukol8435@gmail.com>
2023-10-03 00:14:23 +0530

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+const int ledPin = D2;
+
+void setup() {
+  pinMode(ledPin, OUTPUT); 
+  Serial.begin(115200);    
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  Serial.println("LED ON");   
+  delay(1000);       
+
+  digitalWrite(ledPin, LOW);
+  Serial.println("LED OFF");  
+  delay(1000); 
+}
